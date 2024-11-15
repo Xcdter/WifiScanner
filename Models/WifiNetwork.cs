ce9f117dc4e84f6a3e879517716ac9eb7415dbcd
Xcdter <tigrasha999@ya.ru>
2024-11-15 18:35:20 +0300

    +Models

---
+++ Models/WifiNetwork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiScanner.Models
+{
+    public class WifiNetwork
+    {
+        public string SSID { get; set; }
+        public string BSSID { get; set; }
+        public int SignalStrength { get; set; }
+    }
+}
